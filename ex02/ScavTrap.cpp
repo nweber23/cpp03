f523961ba612ba82a98dd37d13cbd5f57489e5fe
Niklas Weber <nweber@student.42Heilbronn.de>
2025-10-10 12:48:12 +0200

    init ex02

---
+++ ex02/ScavTrap.cpp
@@ -0,0 +1,51 @@
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() {
+	_name = "Unnamed";
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap " << _name << " constructed (default  values)" << std::endl;
+}
+
+ScavTrap::ScavTrap(const std::string& name) : ClapTrap(name) {
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap " << _name << " constructed" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other) {
+	// Copy constructor of Claptrap handels the copying
+	std::cout << "ScavTrap " << _name << " copy constructed" << std::endl;
+}
+
+ScavTrap& ScavTrap::operator=(const ScavTrap& other) {
+	if (this != &other)
+		ClapTrap::operator=(other);
+	std::cout << "ScavTrap " << _name << " copy assigned" << std::endl;
+	return *this;
+}
+
+ScavTrap::~ScavTrap() {
+	std::cout << "ScavTrap " << _name << " destructed" << std::endl;
+}
+
+void ScavTrap::attack(const std::string& target) {
+	if (_hitPoints == 0) {
+		std::cout << "ClapTrap " << _name << " can't attack, it has no hit points left." << std::endl;
+		return;
+	}
+	if (_energyPoints == 0) {
+		std::cout << "ClapTrap " << _name << " can't attack, it has no energy left." << std::endl;
+		return;
+	}
+	--_energyPoints;
+	std::cout << "ClapTrap " << _name << " attacks " << target
+			  << ", causing " << _attackDamage << " points of damage! "
+			  << "(EP left: " << _energyPoints << ")" << std::endl;
+}
+
+void ScavTrap::guardGate() {
+	std::cout << "ScavTrap " << _name << " is now in Gate keeper mode" << std::endl;
+}
